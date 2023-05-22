73ae0fe5f51692dd595738dcfddca2765ae2094d
ericsilberman <esilberm@ucsd.edu>
2023-05-21 22:19:25 -0400

    Final Version
    - need to upload website url for grading

--- sw.js
+++ sw.js
@@ -34,7 +34,14 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.respondWith(caches.open(CACHE_NAME).then(async(cache) => {
+    // Respond with the image from the cache or from the network
+    return cache.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+        // Return the network response
+        return fetchedResponse;
+      });
+    });
+  }));
 });
